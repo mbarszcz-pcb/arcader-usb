8a646d37e329c702b659bcca32991aec6ae994b8
mbarszcz-pcb <62184001+mbarszcz-pcb@users.noreply.github.com>
2021-12-08 21:46:16 -0500

    Update DaemonBiteArcadeEncoderArcadeR-1.0.ino
    Fixed missing * in comments.

--- firmware/DaemonBiteArcadeEncoderArcadeR-1.0/DaemonBiteArcadeEncoderArcadeR-1.0.ino
+++ firmware/DaemonBiteArcadeEncoderArcadeR-1.0/DaemonBiteArcadeEncoderArcadeR-1.0.ino
@@ -1,28 +1,29 @@
-//*  DaemonBite Arcade Encoder
-    Author: Mikael Norrgård <mick@daemonbite.com>
-
-    Copyright (c) 2020 Mikael Norrgård <http://daemonbite.com>
-
-    GNU GENERAL PUBLIC LICENSE
-    Version 3, 29June 2007
- 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-    Modified for use in the ArcadeR USB
-    Matt Barszcz 2021
-    Version 1.0
-*/
+/*  DaemonBite Arcade Encoder
+ *  Author: Mikael Norrgård <mick@daemonbite.com>
+ *
+ *  Copyright (c) 2020 Mikael Norrgård <http://daemonbite.com>
+ *  
+ *  GNU GENERAL PUBLIC LICENSE
+ *  Version 3, 29 June 2007
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  
+ *
+ *  Modified for use in the ArcadeR USB
+ *  Matt Barszcz 2021
+ *  Version 1.0
+ */
 
 #include "Gamepad.h"
 #include <EEPROM.h>			// EEPROM library to save button sync state
